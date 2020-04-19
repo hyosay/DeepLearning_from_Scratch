75925b48d5a99eb6c472645d37086fa3c21bd6c5
hyoseong-j <gytjdwjs@cu.ac.kr>
2020-04-19 19:30:21 +0900

    2020.04.19

--- baekjoon/BJ/BJ.10871.py
+++ baekjoon/BJ/BJ.10871.py
@@ -1,3 +1,15 @@
-a = []
-a = list(map(int,input().split()))
-print(a)
+N, X = map(int,input().split())
+
+
+A = list(map(int,input().split()))
+
+for i in range(0,N):
+    if X > A[i]:
+        print(A[i],end=' ')
+
+
+
+B = list(map(int,input().split()))
+C = map(int,input().split())
+print(B)
+print(C)
