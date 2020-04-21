f9c6202feec2d8c57f45a276b940ebd092ef7b28
hyoseong-j <gytjdwjs@cu.ac.kr>
2020-04-21 22:52:08 +0900

    2020.04.21

--- DeepLearning_from_Scratch/79.p.py
+++ DeepLearning_from_Scratch/79.p.py
@@ -1,8 +1,3 @@
-def adjacent(inputArray):
-    sum1 = sum1()
-
-    for i in range(0, 9):
-        sum1.append(inputArray[i] * inputArray[i + 1])
-    print(sum1)
-inputArray = [1,2,3,4,5,6,7,8,9,10]
-adjacent(inputArray)
+def countBits(n):
+    return n.bit_length()
+print(countBits(14))
