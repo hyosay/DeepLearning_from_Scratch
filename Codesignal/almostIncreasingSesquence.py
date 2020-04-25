81efece59b6833c47a185720cd8579e95123511f
hyoseong-j <gytjdwjs@cu.ac.kr>
2020-04-25 20:01:02 +0900

    2020.04.25

---
+++ Codesignal/almostIncreasingSesquence.py
@@ -0,0 +1,7 @@
+def almostIncreasingSequence(sequence):
+    for i in range(0, len(sequence)):# 1~3
+        sequence()
+        if pow(sequence[len(i)], 2) < pow(sequence[i], 2):
+            seq.append(sequence[i])
+
+    print(seq)
--- DeepLearning_from_Scratch/79.p.py
+++ DeepLearning_from_Scratch/79.p.py
@@ -1,3 +1,21 @@
-def countBits(n):
-    return n.bit_length()
-print(countBits(14))
+sequence = [1, 3, 2]
+
+def almostIncreasingSequence(sequence):
+
+    droppped = False
+    last = prev = min(sequence) - 1#0
+    for i in sequence:
+        if i <= last:
+            if droppped:
+                return False
+            else:
+                droppped = True
+            if i <= prev:
+                prev = last
+            elif i >= prev:
+                prev = last = i
+        else:
+            prev,last = last,i
+    return True
+
+
